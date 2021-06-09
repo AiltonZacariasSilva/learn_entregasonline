5800634a91b92fe1e896d28f1e25cf0da2873c71
Ailton Zacarias da Silva <ailton.z.da.silva@avanade.com>
2021-06-09 19:40:43 -0300

    conexao com o db - ok

--- src/main/java/com/empresa/entregasonline/api/controller/ClienteController.java
+++ src/main/java/com/empresa/entregasonline/api/controller/ClienteController.java
@@ -3,6 +3,9 @@ package com.empresa.entregasonline.api.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,22 +14,16 @@ import com.empresa.entregasonline.domain.model.Cliente;
 @RestController // Identificação de componente spring capaz de tratar requisições http
 public class ClienteController {
 
+	@PersistenceContext
+	private EntityManager manager; 
+	
+	
 	@GetMapping("/clientes") // Mapeamento do método
 	public List<Cliente> listar() {
-		var cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("Maria Jose");
-		cliente1.setEmail("maria.jose@dominio.com.br");
-		cliente1.setTelefone("11 90000-1111");
 
-		var cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("Jose Maria");
-		cliente2.setEmail("jose.maria@dominio.com.br");
-		cliente2.setTelefone("11 91111-2222");		
-		
-		
-		return Arrays.asList(cliente1,cliente2);
+		return manager.createQuery("from Cliente", Cliente.class)
+				.getResultList();
+	
 	}
 	
 }
