5116d353b089617934af5a50a30cad6891e2a279
Ailton Zacarias da Silva <ailton.z.da.silva@avanade.com>
2021-06-09 21:27:26 -0300

    20210609_2127

--- src/main/java/com/empresa/entregasonline/api/controller/ClienteController.java
+++ src/main/java/com/empresa/entregasonline/api/controller/ClienteController.java
@@ -1,29 +1,59 @@
 package com.empresa.entregasonline.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.empresa.entregasonline.domain.model.Cliente;
+import com.empresa.entregasonline.repository.ClienteRepository;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 @RestController // Identificação de componente spring capaz de tratar requisições http
+@RequestMapping("/clientes")
 public class ClienteController {
 
-	@PersistenceContext
-	private EntityManager manager; 
-	
-	
-	@GetMapping("/clientes") // Mapeamento do método
-	public List<Cliente> listar() {
+	private ClienteRepository clienteRepository;
 
-		return manager.createQuery("from Cliente", Cliente.class)
-				.getResultList();
-	
+	@GetMapping // Mapeamento do método
+	public List<Cliente> listar() {
+		return clienteRepository.findAll();
 	}
-	
+
+	@GetMapping("/{clienteId}")
+	public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
+		return clienteRepository.findById(clienteId).map(cliente -> ResponseEntity.ok(cliente))
+				.orElse(ResponseEntity.notFound().build());
+	}
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente adicionar(@RequestBody Cliente cliente) {
+		return clienteRepository.save(cliente);
+	}
+
+	@PutMapping("/{clienteId}")
+	public ResponseEntity<Cliente> atualizar(@PathVariable Long clienteId, @RequestBody Cliente cliente) {
+
+		if (!clienteRepository.existsById(clienteId)) {
+			return ResponseEntity.notFound().build();
+		}
+
+		cliente.setId(clienteId);
+		cliente = clienteRepository.save(cliente);
+
+		return ResponseEntity.ok(cliente);
+
+	}
+
 }
